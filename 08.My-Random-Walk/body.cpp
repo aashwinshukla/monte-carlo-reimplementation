dbd86ed9401cbda64c75dff95ae5718c2300a6b5
aashwinshukla <aashwinshukla585@gmail.com>
2026-08-26 13:49:16 +0530

    body

---
+++ 08.My-Random-Walk/body.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <limit>
+#include <random>
+
+using namespace std;
+
+int main(){
+    int input1
+    
+    while(true){
+        cout << endl;
+        cout << "Random Walk Simulation" << endl;
+        cout << "1. 1 Particle" << endl;
+        cout << "2. 5 Particle" << endl;
+        cout << "3. 10 Particle" << endl;
+        cout << "4. custome " << endl;
+        cout << "5. Default " << endl;
+        cout << "6. Back" << endl;
+        cout << "Choose your choice : " << endl;
+        
+        cin >> input1; 
+        int choose1 = check1(input1);
+    }
+}
